74385881216b238da8eeba512246a0775a905ef7
martinalberto <martinalberto@gmail.com>
2014-05-16 10:55:20 +0000

    fix errors.

---
+++ calefa/utemper/software/utemper/start_read_ldr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\tIniciar start_read_ldr.sh"
+
+count=1
+while [ $count -le 15 ]
+do
+  python read_ldr.py 
+  #x=$(( $x + 1 )) # NUNCA TERMINA!!!!!
+  sleep 30
+  echo $(date  +"%F_%T")";4;start_read_ldr.sh;salida inesperada de read_ldr.py KO">>/var/utemp/logs.log
+done
+
+echo $(date  +"%F_%T")";5;start_read_ldr.sh;max errores KO">>/var/utemp/logs.log
+echo $(date  +"%F_%T")";5;start_read_ldr.sh;Rebbot OK">>/var/utemp/logs.log
+sleep 60
+
+reboot
--- calefa/utemper/software/utemper/start_utemper.sh
+++ calefa/utemper/software/utemper/start_utemper.sh
@@ -6,13 +6,13 @@ echo "Iniciar Utemper"
 mkdir -p /var/utemp/
 echo "-1" > /tmp/rele.var
 echo "-1" > /tmp/temp.var
-echo "-1" > /tmp/lrd.var
+echo "-1" > /tmp/ldr.var
 echo "0" > /tmp/time_espera.var
 # poner permisos.
 
 chmod 777 /tmp/rele.var
 chmod 777 /tmp/temp.var
-chmod 777 /tmp/lrd.var
+chmod 777 /tmp/ldr.var
 chmod 777 /tmp/time_espera.var
 
 #logs
@@ -21,7 +21,11 @@ echo $(date  +"%F_%T")";1;INI1;Iniciamos ficheros">>/var/utemp/logs.log
 chmod 777 /var/utemp/logs.log
 
 # Iniciamos programas.
+sleep 10
 
-start_read_temp.sh
+bash /home/pi/utemper/start_read_temp.sh &
+bash /home/pi/utemper/start_read_ldr.sh &
+bash /home/pi/utemper/start_rele.sh &
 sleep 2
+
 
