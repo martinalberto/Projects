de258b129d308af9496cda2cf3986fab3ff4e44c
martinalberto <martinalberto@gmail.com>
2014-09-30 19:29:32 +0000

    ficheros

--- calefa/utemper/software/utemper/utemper_screen.py
+++ calefa/utemper/software/utemper/utemper_screen.py
@@ -9,24 +9,24 @@ from utemper_public import *
 class cScreen:
     
     pantalla = 0
-    suceso_estado = 0
     screen_number = 0
     carpeta_img = "img/dia/"
     letra_color = (0,0,0)
     Letra_top = None
-    lastTimeRefes =0
+    lastTimeRefes = time.time() + 5
     
     def __init__(self):
-        #try:
-            
+        try:
             # iniciamos la pantalla.
             os.putenv('SDL_VIDEODRIVER', 'fbcon')
             os.putenv('SDL_FBDEV'      , '/dev/fb1')
+            os.putenv('SDL_MOUSEDRV' , 'TSLIB')
+            os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
             pygame.init()
             #screen = pygame.display.set_mode((gv.screen_widht, gv.screen_widht))
             self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
             pygame.display.set_caption("utemper")
-            pygame.mouse.set_visible(False)
+            #pygame.mouse.set_visible(False)
 
             # inicializamos las letras.
             self.Letra_top = pygame.font.Font("font/stag-sans-light-webfont.ttf", 20)
@@ -39,27 +39,18 @@ class cScreen:
             gv.screen_height  = pygame.display.Info().current_h
             
             self.refrescar_screen()
-        #except:
-            #clog().log(4,"Imposible iniciar la pantalla")
-            #self.pantalla=0
+        except:
+            clog().log(4,"Imposible iniciar la pantalla")
+            self.pantalla=0
 
         
     def suceso(self):
         if(self.pantalla==1):
-            if (self.suceso_estado== 0  ):
-                self.suceso_estado+=1
-                
-                # Check el estado del mouse!!!!
-                self.check_screen()
-            elif self.suceso_estado== 1:
-                self.suceso_estado+=1
-                if (time.time()-self.lastTimeRefes > 60 ):
-                    self.lastTimeRefes = time.time()
-                    self.refrescar_screen()  
-            elif (self.suceso_estado== 2  ):
-                self.suceso_estado+=1
-            else:
-                self.suceso_estado=0
+            # Check el estado del mouse!!!!
+            self.check_screen()
+            if (time.time()-self.lastTimeRefes > 60 ):
+                self.lastTimeRefes = time.time()
+                self.refrescar_screen()  
     
     def reset():
         self.refrescar_screen()
@@ -68,6 +59,7 @@ class cScreen:
         for event in pygame.event.get():
             if(event.type is MOUSEBUTTONDOWN):
                  pos = pygame.mouse.get_pos()
+                 print pygame.mouse.get_pos()
                  clog().log(1,"BOTON press screen %d x %d  y." %(pos[0], pos[1]))
                  if(self.pantalla==0):
                        return 0
@@ -77,14 +69,16 @@ class cScreen:
                        self.boton_screen_1(pos)
                  else:
                        self.boton_screen_0(pos)
+                 self.refrescar_screen()
 
     def refrescar_screen(self):
+        self.lastTimeRefes = time.time()
         if(self.pantalla==0):
 			return 0
         if self.screen_number == 0:
 			self.refrescar_screen_0()
         elif self.screen_number == 1:
-			self.refrescar_screen_0()
+			self.refrescar_screen_1()
         else:
 			elf.refrescar_screen_0()
             
@@ -104,7 +98,7 @@ class cScreen:
         fondo_up = pygame.image.load(fichero_up).convert_alpha()
         self.screen.blit(fondo_up, (0, -7))
         #dia y hora:
-        string=time.strftime("%d-%b-%Y %H:%M", time.localtime())
+        string=time.strftime("%d-%b %I:%M%p", time.localtime())
         mytext = self.Letra_top.render(string, False, self.letra_color).convert_alpha()
         posX= gv.screen_widht -(mytext.get_size()[0] + 15)
         self.screen.blit(mytext, (posX, 10))
@@ -146,33 +140,35 @@ class cScreen:
         fichero_fondo = self.carpeta_img+"fondo/change_temp.jpg"
         fondo = pygame.image.load(fichero_fondo).convert()
         self.screen.blit(fondo, (0, 0))
-
-        #iconos
-        posX = 10
-        fichero_icono = self.carpeta_img+"iconos/subir_temp.png"
-        icon = pygame.image.load(fichero_icono).convert_alpha()
-        self.screen.blit(icon, (20,100))
-        fichero_icono = self.carpeta_img+"iconos/bajar_temp.png"
-        icon = pygame.image.load(fichero_icono).convert_alpha()
-        self.screen.blit(icon, (120,100))
-
-        fichero_icono = self.carpeta_img+"iconos/boton_config.png"
-        icon = pygame.image.load(fichero_icono).convert_alpha()
-        posX = gv.screen_widht - icon.get_size()[0] - 5
-        posY = (gv.screen_height) - (icon.get_size()[1]/2) -5
-        self.screen.blit(icon, (posX,posY))
-
-        fichero_icono = self.carpeta_img+"iconos/boton_volver.png"
-        icon = pygame.image.load(fichero_icono).convert_alpha()
-        posY = (gv.screen_height) - (icon.get_size()[1]/2) -5
-        self.screen.blit(icon, (15,posY))
+        
+        #temp
+        string="%.1f" %gv.temperatura_max
+        mytext = self.Letra_temp2.render(string, False, self.letra_color).convert_alpha()
+        posX = (gv.screen_widht/2) - (mytext.get_size()[0]/2)
+        posY = (gv.screen_height/2)- (mytext.get_size()[1]/2)
+        self.screen.blit(mytext, (posX ,posY))
+         
+		# se muestran lo cambios en pantalla
+        pygame.display.flip()
+        clog().log(1,"Refrescar screen 1 OK")
 
     def boton_screen_0 (self, pos):
         # Entrar menu
         self.screen_number = 1
-        self.refrescar_screen()
         
-    def botton_screen_1 (self, pos):
-        # subir boton
-        if (pos[0] < (gv.screen_widht/2)-10) and (pos[1] < 120):
-          print ""                
+    def boton_screen_1(self, pos):
+        # subir boton 
+        # pos[0] x 
+        # pos[1] y
+        if (pos[0] < 100) and (60 < pos[1] < 160):  # bajo temp
+             self.changeTemp(gv.temperatura_max+0.5)
+        elif (pos[0] > 180) and (60 < pos[1] < 160):  # subir temp
+             self.changeTemp(gv.temperatura_max-0.5)
+        elif (pos[0] < 80) and ( pos[1] > 170):  # volver
+             self.screen_number = 0
+			
+	
+    def changeTemp(self, new_temp):
+        if (15 < new_temp <35):
+          gv.temperatura_max = new_temp
+          gv.saveConfigurcion = True
